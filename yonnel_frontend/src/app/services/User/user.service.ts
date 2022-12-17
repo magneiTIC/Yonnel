8e4909f36cfe65eaeeee9b10cf851eb747d13fc9
adnGogo <ndiayeawa3107@gmail.com>
2022-12-17 07:00:03 +0000

    adn2

--- yonnel_frontend/src/app/services/User/user.service.ts
+++ yonnel_frontend/src/app/services/User/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private httpClient:HttpClient
+    ) { }
+url='http://localhost:3000/users/'
+
+  getAllUser(){
+    return this.httpClient
+      .get<any>(this.url+'')
+      .pipe(
+        map((userData: any )=>{
+         return userData
+        })
+ 
+       )
+  }
 }
--- yonnel_frontend/src/app/views/Admin/dashboard/dashboard.component.ts
+++ yonnel_frontend/src/app/views/Admin/dashboard/dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AgenceService } from 'src/app/services/Agence/agence.service';
+import { PaysService } from 'src/app/services/Pays/pays.service';
+import { UserService } from 'src/app/services/User/user.service';
+import { SousAgenceService } from 'src/app/services/SousAgence/sous-agence.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  totalAgence:any
+  totalSousAgence:any
+  totalUser:any
+  totalPays:any
 
-  constructor() { }
+  constructor(
+    private agenceService: AgenceService,
+    private paysServce:PaysService,
+    private sousAgenceService:SousAgenceService,
+    private userService:UserService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.sousAgenceService.getAllSousAgence().subscribe(data=>{
+      this.totalSousAgence=data.total;
+      console.log(this.totalAgence)
+      })
+
+      this.agenceService.getAllAgence().subscribe(data=>{
+        this.totalAgence=data.total;
+        console.log(this.totalAgence)
+        })
+        this.userService.getAllUser().subscribe(data=>{
+          this.totalUser=data.total-1;
+          console.log(this.totalUser)
+          })
+          this.paysServce.getAllPays().subscribe(data=>{
+            this.totalPays=data.total;
+            console.log(this.totalPays)
+            })
+
   }
+  
 
 }
